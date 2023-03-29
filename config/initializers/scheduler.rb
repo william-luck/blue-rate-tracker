07274ff36cd464bad60ba6b178f6bd8f9d78b9d8
William Luck <luck.william.h@gmail.com>
2023-03-29 15:08:43 -0300

    complete scheduler, set to update every day at 6:00 AM AST

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,30 +4,28 @@ require 'net/http'
 require 'json'
 
 
-s = Rufus::Scheduler.singleton
+scheduler = Rufus::Scheduler.singleton
 
-s.every '5s' do
+# Every day at 9:00 AM UTC (6:00 AM Argentina Standard Time)
+scheduler.cron '00 09 * * *' do
 
     # Returns last two days of data for blue rate
     url = 'https://api.bluelytics.com.ar/v2/evolution.json?days=4'
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
-    # puts result['blue']['value_avg']
 
-
+    # Calculates change in price
     current_value_avg = (result[1]['value_sell'] + result[1]['value_buy']) / 2
     prev_value_avg = (result[3]['value_sell'] + result[3]['value_buy']) / 2
     change_ratio = current_value_avg/prev_value_avg
 
-    product = Product.first
-    base_price = product.price
-    new_price = base_price * change_ratio
-    product.update!(price: new_price.to_i)
-    
-
-    # I want to see the day before price, and the new price
-    
-
+    # Updates each product price according to change ratio
+    products = Product.all
+    products.each{ |product| 
+        base_price = product.price
+        new_price = base_price * change_ratio
+        product.update!(price: new_price.to_i)
+    }
 
 end
