a71fd933dd7fc43a0f0f7c5e4f5f9d40b0d2c9c4
William Luck <luck.william.h@gmail.com>
2023-03-29 14:25:50 -0300

    add scheduler code for updating one product, test

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -8,12 +8,26 @@ s = Rufus::Scheduler.singleton
 
 s.every '5s' do
 
-    url = "https://api.bluelytics.com.ar/v2/latest"
+    # Returns last two days of data for blue rate
+    url = 'https://api.bluelytics.com.ar/v2/evolution.json?days=4'
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
-    puts result['blue']['value_avg']
+    # puts result['blue']['value_avg']
 
-# #   Rails.logger.info "hello, it's #{Time.now}"
-# #   Rails.logger.flush
+
+    current_value_avg = (result[1]['value_sell'] + result[1]['value_buy']) / 2
+    prev_value_avg = (result[3]['value_sell'] + result[3]['value_buy']) / 2
+    change_ratio = current_value_avg/prev_value_avg
+
+    product = Product.first
+    base_price = product.price
+    new_price = base_price * change_ratio
+    product.update!(price: new_price.to_i)
+    
+
+    # I want to see the day before price, and the new price
+    
+
+
 end
